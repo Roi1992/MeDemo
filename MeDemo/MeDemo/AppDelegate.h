c638e605554ca858b0c309bd24b5dc4ba0146ac6
 四十九 <840539649@qq.com>
2016-04-17 22:44:43 +0800

    四十九
    iOS Demo 初始化

---
+++ MeDemo/MeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeDemo
+//
+//  Created by WSF on 16/4/17.
+//  Copyright © 2016年 WSF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MeDemo/MeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeDemo
+//
+//  Created by WSF on 16/4/17.
+//  Copyright © 2016年 WSF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
